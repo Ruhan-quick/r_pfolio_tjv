55861ef34ca5d685056fc76d4beda6869c69e9ee
ruhan-quick <ruhanquick@gmail.com>
2024-11-17 15:34:09 +0600

    Working on projects

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -14,6 +14,7 @@ const About = () => {
     }
     return (
         <section className="c-space my-20">
+            <p className="head-text">About</p>
             <div className="grid xl:grid-cols-3 xl:grid-rows-6 md:grid-cols-2 grid-cols-1 gap-5 h-full">
                 <div className="col-span-1 xl:row-span-3">
                     <div className="grid-container">
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -2,8 +2,8 @@ import {Canvas} from "@react-three/fiber";
 import {PerspectiveCamera} from "@react-three/drei";
 import {Suspense} from "react";
 import CanvasLoader from "../components/CanvasLoader.jsx";
-import HackerRoom from "../components/HackerRoom.jsx";
-// import {Leva, useControls} from "leva";
+// import HackerRoom from "../components/HackerRoom.jsx";
+import {Leva, useControls} from "leva";
 import {useMediaQuery} from "react-responsive";
 import {calculateSizes} from "../constants/index.js";
 import Target from "../components/Target.jsx";
@@ -12,72 +12,73 @@ import Cube from "../components/Cube.jsx";
 import Rings from "../components/Rings.jsx";
 import HeroCamera from "../components/HeroCamera.jsx";
 import Button from "../components/Button.jsx";
-import HelloDesk from "../components/HelloDesk.jsx";
+// import HelloDesk from "../components/HelloDesk.jsx";
+import SciTable from "../components/SciTable.jsx";
 const Hero = () => {
     const isSmall = useMediaQuery({maxWidth:440})
     const isMobile = useMediaQuery({minWidth:440,maxWidth: 768})
     const isTablet = useMediaQuery({maxWidth: 1024, minWidth:768})
     const sizes = calculateSizes(isSmall, isMobile, isTablet);
-    // const x = useControls(
-    //     'Hacker Room', {
-    //         positionX:{
-    //             value: 2.5,
-    //             min: -10,
-    //             max: 10
-    //         },
-    //         positionY:{
-    //             value: 2.5,
-    //             min: -10,
-    //             max: 10
-    //         },
-    //         positionZ:{
-    //             value: 2.5,
-    //             min: -10,
-    //             max: 10
-    //         },
-    //         rotationX:{
-    //             value: 1,
-    //             min: -10,
-    //             max: 10
-    //         },
-    //         rotationY:{
-    //             value: 1,
-    //             min: -10,
-    //             max: 10
-    //         },
-    //         rotationZ:{
-    //             value: 1,
-    //             min: -10,
-    //             max: 10
-    //         },
-    //         scale:{
-    //             value:1,
-    //             min: 0.1,
-    //             max: 10
-    //         }
-    //     }
-    // );
+    const x = useControls(
+        'Hacker Room', {
+            positionX:{
+                value: 51,
+                min: -80,
+                max: 80
+            },
+            positionY:{
+                value: -10.8,
+                min: -20,
+                max: 20
+            },
+            positionZ:{
+                value: 45.0,
+                min: -20,
+                max: 70
+            },
+            rotationX:{
+                value: 0.04,
+                min: -2,
+                max: 2
+            },
+            rotationY:{
+                value: -0.32,
+                min: -2,
+                max: 2
+            },
+            rotationZ:{
+                value: 0.00,
+                min: -2,
+                max: 2
+            },
+            scale:{
+                value:1.80,
+                min: 0.1,
+                max: 5
+            }
+        }
+    );
     // border-2 border-blue-500
     return (
         <section className="min-h-screen  w-full flex flex-col relative">
             <div className="w-full mx-auto flex flex-col sm:mt-28 mt-16 c-space gap-3">
-                <p className="sm:text-3xl text-2xl font-medium text-white text-center font-generalsans">Assalamualaikum, I'm Ruhan <span className="waving-hand">Hi</span> </p>
+                <p className="sm:text-3xl text-2xl font-medium text-white text-center font-generalsans">Assalamualaikum, I&apos;m Ruhan <span className="waving-hand">Hi</span> </p>
                 <p className="hero_tag text-gray_gradient">Building Products & Brands</p>
 
             </div>
 
             <div className="w-full h-full absolute inset-0">
-                {/*<Leva/>*/}
+                <Leva/>
                     <Canvas className="w-full h-full">
                         <Suspense fallback={<CanvasLoader/>}>
                             <PerspectiveCamera makeDefault position={[0, 0, 20]} />
 
                             <HeroCamera isMobile={isMobile}>
-                                <HackerRoom
+                                <SciTable
                                     // scale={0.07}
-                                    position={sizes.deskPosition}
-                                    rotation={[0, -Math.PI, 0]}
-                                    scale={sizes.deskScale}
+                                    position={[x.positionX, x.positionY, x.positionZ]}
+                                    rotation={[x.rotationX, x.rotationY, x.rotationZ]}
+                                    scale={[x.scale,x.scale,x.scale]}
                                 />
                                 {/*<HelloDesk*/}
                                 {/*    position={sizes.deskPosition}*/}
